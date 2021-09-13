cdbf3c597b3050482768d052b8277846bc55daeb
Zsigil <chickpox@freemail.hu>
2021-09-13 17:25:57 +0200

    creating models after creating dbase in phpadmin

---
+++ app/Models/GuestEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class GuestEntry extends Model
+{
+    protected $table = "guest_entry";
+    protected $fillable = ["full_name", "email", "comment"];
+}
+
+?>
---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = "user";
+    protected $fillable = ["name","email", "password"];
+}
+
+?>
