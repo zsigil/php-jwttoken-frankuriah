b694c2bbe1c2c117767a854dc58647512c302f32
Zsigil <chickpox@freemail.hu>
2021-09-13 09:56:19 +0200

    database config

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+$database_config = [
+    "driver"=>"mysql",
+    "database"=>"slim4_frankuriah",
+    "host"=>"127.0.0.1",
+    "username"=>"root",
+    "password"=>"",
+    "charset"=>"utf8",
+    "collision"=>"utf8_unicode_ci",
+    "prefix"=>""
+];
+
+return $database_config;
+?>
