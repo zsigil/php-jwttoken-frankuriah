4aa6b1ad66821cdc97d6d18255a351689059d5d5
Zsigil <chickpox@freemail.hu>
2021-09-13 17:38:51 +0200

    creating first guest entry

--- app/Controllers/GuestEntryController.php
+++ app/Controllers/GuestEntryController.php
@@ -2,6 +2,7 @@
 
 namespace app\Controllers;
 
+use app\Models\GuestEntry;
 use app\Requests\CustomRequestHandler;
 use app\Response\CustomResponse;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -10,15 +11,26 @@ use Psr\Http\Message\RequestInterface as Request;
 class GuestEntryController
 {
     protected $customResponse;
+    protected $guestEntry;
 
     public function __construct()
     {
         $this->customResponse = new CustomResponse();
+        $this->guestEntry = new GuestEntry();
     }
 
     public function createGuest(Request $request, Response $response){
-        $name = CustomRequestHandler::getParam($request, "name");
-        $responseMessage = $name;
+        // $name = CustomRequestHandler::getParam($request, "name");
+        // $responseMessage = $name;
+        // return $this->customResponse->is200Response($response, $responseMessage);
+
+        $this->guestEntry->create([
+            "full_name"=>CustomRequestHandler::getParam($request, "full_name"),
+            "email"=>CustomRequestHandler::getParam($request, "email"),
+            "comment"=>CustomRequestHandler::getParam($request, "comment"),
+        ]);
+
+        $responseMessage = "Guest entry data created successfully";
         return $this->customResponse->is200Response($response, $responseMessage);
     }
 }
--- config/middleware.php
+++ config/middleware.php
@@ -2,6 +2,7 @@
     use Slim\App;
 
     return function(App $app){
+        $app->getContainer()->get('settings');
         $app->addRoutingMiddleware();
         $app->addErrorMiddleware(true,true,true);
     }
